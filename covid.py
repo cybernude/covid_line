6eac6d86ea438967a8d5ee836c885af5ae5357f0
sukkla01 <sukkla01@gmail.com>
2020-04-14 11:02:52 +0700

    1

--- covid.py
+++ covid.py
@@ -20,8 +20,8 @@ def lineNotify():
 def _lineNotify(payload,file=None):
     import requests
     url = 'https://notify-api.line.me/api/notify'
-    # token = '88XWX6YhoPaQmVKZ5GERV9Xq3NZMqaljU2rAcOv91yz'	#ญาติโก
-    token = 'Lhv3WyVSehM1QlKyCUW2WeKggvG34hT7t9LSEFUrA8T'	#q รพ
+    # token = ''	#ญาติโก
+    token = ''	#q รพ
 
     headers = {'Authorization':'Bearer '+token}
     return requests.post(url, headers=headers , data = payload, files=file)

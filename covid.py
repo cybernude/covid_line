5f06182078bdff950125a014da73e83ac2c4d678
sukkla01 <sukkla01@gmail.com>
2020-04-14 11:01:40 +0700

    first commit

---
+++ covid.py
@@ -0,0 +1,35 @@
+import requests
+import schedule
+import time
+
+response = requests.get("https://covid19.th-stat.com/api/open/today")
+data =  response.json()
+
+def lineNotify():
+    message = 'สถานการณ์การระบาดโควิด19ในประเทศไทย \n'\
+    'ติดเชื้อสะสม : ' + str(data["Confirmed"]) + ' ราย\n'+'หายแล้ว : ' + str(data["Recovered"]) + ' ราย\n' \
+                            'รักษาอยู่ใน รพ : ' + str(data["Hospitalized"]) + ' ราย\n' \
+                            'ตายทั้งหมด : ' + str(data["Deaths"]) + ' ราย\n' \
+                            'ตายเพิ่ม : ' + str(data["NewDeaths"]) + ' ราย\n' \
+                            'ติดเชื่อเพิ่ม : ' + str(data["NewConfirmed"]) + ' ราย\n' \
+                            'อัพเดทล่าสุด : ' + str(data["UpdateDate"]) + ' \n' \
+                            'ข้อมูลจาก : https://covid19.th-stat.com/th (กรมควบคุมโรค)' 
+    payload = {'message': message}
+    return _lineNotify(payload)
+
+def _lineNotify(payload,file=None):
+    import requests
+    url = 'https://notify-api.line.me/api/notify'
+    # token = '88XWX6YhoPaQmVKZ5GERV9Xq3NZMqaljU2rAcOv91yz'	#ญาติโก
+    token = 'Lhv3WyVSehM1QlKyCUW2WeKggvG34hT7t9LSEFUrA8T'	#q รพ
+
+    headers = {'Authorization':'Bearer '+token}
+    return requests.post(url, headers=headers , data = payload, files=file)
+
+schedule.every().day.at("13:00").do(lineNotify)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
